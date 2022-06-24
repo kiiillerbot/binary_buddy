bf488bdffef8f83e9f9f0c954ec13e34d4bdbea9
kiiillerbot <dev.shimolkhan@gmail.com>
2022-06-25 00:43:49 +0800

    Updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+  end
+
   root to: 'pages#index'
   get 'notes', to: "pages#notes", as: :notes
   get 'videos', to: "pages#videos", as: :videos
