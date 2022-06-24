53b7b5ee2350c5cde412a277e124e6870ff12de7
kiiillerbot <dev.shimolkhan@gmail.com>
2022-06-25 00:17:59 +0800

    Finish

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def videos
+  end
+
+  def notes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#index'
+  get 'notes', to: "pages#notes", as: :notes
+  get 'videos', to: "pages#videos", as: :videos
 end
